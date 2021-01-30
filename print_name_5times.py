b6c0b443b3b2d647b5ab1cd1e7616b61c46d99bc
Soumya-CET <soumyaranjanmohanty.cetb@gmail.com>
2021-01-30 10:24:27 +0530

    1st commit

---
+++ print_name_5times.py
@@ -0,0 +1,14 @@
+#without using loop
+print('soumya\n'*5)
+
+#using forloop
+for i in range(0,5):
+    print('soumya',end='\n')
+
+print('\n')
+
+#using while loop
+i=0
+while i!=5 :
+    print('soumya',end='\n')
+    i+=1
